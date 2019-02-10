4b0288f5d68bf68523893e2cbd1750e2ff7f788a
Kamil <xkamilwrobel@gmail.com>
2019-02-10 19:50:44 +0100

    Add swagger and fix authorization

--- src/main/java/com/canx/postapp/configuration/WebSecurityConfig.java
+++ src/main/java/com/canx/postapp/configuration/WebSecurityConfig.java
@@ -1,41 +1,66 @@
 package com.canx.postapp.configuration;
 
+import com.canx.postapp.dto.UserDTO;
+import com.canx.postapp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+import java.util.Optional;
 
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public WebSecurityConfig(UserRepository userRepository) {
+        this.userRepository = userRepository;
+        init();
+    }
+
+    private void init(){
+        BCryptPasswordEncoder bcrypt = new BCryptPasswordEncoder();
+        userRepository.save(new UserDTO("admin",bcrypt.encode("admin123")));
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/api/users/**").permitAll()
+                .antMatchers("/api/users/").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
-
     }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(s -> null)
-                .and()
-                .inMemoryAuthentication()
-                .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN")
-                .and()
-                .withUser("user").password(passwordEncoder().encode("user")).roles("USER");
+        auth.userDetailsService(this);
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        String errorMessage = String.format("User %s not found", username);
+        Optional<UserDTO> user = userRepository.findByUsername(username);
+
+        if (user.isPresent()) {
+            return new UserDetail(user.get());
+        } else {
+            throw new UsernameNotFoundException(errorMessage);
+        }
     }
 }
